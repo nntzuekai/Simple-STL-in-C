28dd2579b079c0b39a369e14754b18eab6dbbf52
nntzuekai <nntzuekai@gmail.com>
2017-05-12 17:40:21 +0800

    0.1.0
    Use _Generic to overload functions

--- main.c
+++ main.c
@@ -1,33 +1,49 @@
 #include<stdio.h>
 #include<math.h>
 #include"list.h"
-void l_print(Item *t){
-    if(t)printf("%.2f  %.2f\n",t->coe,t->pow);
+void l_print(Item *t) {
+    if(t)printf("%.2f  %.2f\n", t->coe, t->pow);
     else puts("NULL");
 }
-void l_scan(Item *t){
-    if(t)scanf("%lf%lf",&t->coe,&t->pow);
-    else puts("NULL");
-}
-int main(void){
-    List l1,l2;
-    Item t;
-    list_create(&l1,NULL);
-    list_create(&l2,NULL);
-
-    for(int i=0;i<20;++i){
-        t.coe=t.pow=i;
-        list_push_back(&l1,t);
+void l_scan(Item *t) {
+    if (t) {
+        scanf("%lf", &t->coe);
+        t->pow = 1;
     }
-    t.coe=t.pow=10;
-    list_assign(&l2,&l1);
-    list_splice2(&l2,list_find(&l2,&(Item){5,5}),&l2,list_find(&l2,&(Item){6,6}),list_find(&l2,&(Item){10,10}));
-    puts("Forward:");
-    list_for_all(&l2,l_print);
-    puts("Backward:");
-    list_for_all_backward(&l2,l_print);
-    //puts("L2:");
-    //list_for_all_backward(&l1,l_print);
-    printf("%u  %u\n",l1.count,l2.count);
+    else puts("NULL");
+}
+int l_cmp(const Item *t1, const Item *t2) {
+    if(t1->pow != t2->pow) {
+        return t1->pow > t2->pow ? 1 : -1;
+    }
+    else if(t1->coe != t2->coe) {
+        return t1->coe > t2->coe ? 1 : -1;
+    }
+    else return 0;
+}
+int main(void) {
+    List l1, l2;
+    int n;
+
+    create(l1, l_cmp);
+    create(l2, (Comparer)NULL);
+
+    scanf("%d", &n);
+    resize(l1, n);
+    for_all(l1, l_scan);
+
+    //list_for_all(&l1, l_print);
+    sort(l1);
+
+    for_all(l1, l_print);
+    putchar('\n');
+    for_each_r( rend(l1), rbegin(l1), l_print);
+
+    printf("%u  %u\n", size(l1), size(l2));
+
+    assign(l2,l1);
+    unique(l2);
+    for_all_r(l2,l_print);
+
     return 0;
 }
